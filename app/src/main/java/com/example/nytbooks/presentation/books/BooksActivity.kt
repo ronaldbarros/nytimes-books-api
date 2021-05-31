ff62d8fa948f7d5190c2b5566be50c507c67c5c5
Ronald Barros <rsmbarros@gmail.com>
2021-05-30 23:30:58 -0300

    add toolbar

--- app/src/main/java/com/example/nytbooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nytbooks/presentation/books/BooksActivity.kt
@@ -2,11 +2,18 @@ package com.example.nytbooks.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import com.example.nytbooks.R
+
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        toolbar.title = "Books"
+        setSupportActionBar(toolbar)
     }
 }
